8fc526dd125cb708c9eee922ca22740afd08a149
trevismcgill <trevismcgill25@gmail.com>
2020-10-13 12:22:54 -0500

    Stubbed out Sessions create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,11 @@
 class SessionsController < ApplicationController
+
+    def create
+        if @user = User.find_by(email: params[:user][:email])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            redirect_to new_sessions_path
+        end
+    end
 end
