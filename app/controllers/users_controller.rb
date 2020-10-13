2b3fccca1f2b245aaa7f50377571e6024ddf8a17
trevismcgill <trevismcgill25@gmail.com>
2020-10-13 12:04:31 -0500

    User sign up successfully creates a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,16 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
+    def create
+        binding.pry
+        @user = User.create(user_params)
+        redirect_to user_path(@user)
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password, :dungeon_master)
+    end
 end
