25b6e806462a61aa9ddd8604a3fdc174504cfd9c
trevismcgill <trevismcgill25@gmail.com>
2020-10-13 10:22:53 -0500

    Set up User routes, migration, and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_secure_password
+    validates :username, presence: true
+    validates :email, presence: true
+    has_many :
+    has_many : through: :
 end
