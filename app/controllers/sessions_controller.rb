3966202433a70b5f353bc92f945352a2276e5da3
trevismcgill <trevismcgill25@gmail.com>
2020-10-14 11:30:54 -0500

    Established functional OmniAuth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,43 @@
 class SessionsController < ApplicationController
 
     def create
-        @user = User.find_by(email: params[:user][:email])
-        if @user && @user.authenticate(params[:user][:password])
-            session[:user_id] = @user.id
-            redirect_to user_path(@user)
+        binding.pry
+        if auth
+            if @user = User.find_by(email: auth[:info][:email])
+                session[:user_id] = @user.id
+                redirect_to user_path(@user)
+            else
+                @user = User.new
+                @user.email = auth[:info][:email]
+                @user.username = auth[:info][:nickname]
+                @user.password = SecureRandom.hex
+                if @user.save
+                    session[:user_id] = @user.id
+                    redirect_to user_path(@user)
+                else
+                    redirect_to login_path
+                end
+            end
         else
-            redirect_to login_path
+            @user = User.find_by(email: params[:user][:email])
+            if @user && @user.authenticate(params[:user][:password])
+                session[:user_id] = @user.id
+                redirect_to user_path(@user)
+            else
+                redirect_to login_path
+            end
         end
+        
     end
 
     def destroy
         session.delete("user_id")
         redirect_to login_path
+    end
+
+    private
+ 
+    def auth
+        request.env['omniauth.auth']
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
+  get '/auth/github/callback', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
 
