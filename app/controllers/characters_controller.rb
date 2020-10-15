004358bf9151ba677a938923097157db53918c8c
trevismcgill <trevismcgill25@gmail.com>
2020-10-15 17:33:54 -0500

    Stubbed out actions for Characters and Campaigns

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,30 @@
 class CharactersController < ApplicationController
+    def new
+
+    end
+
+    def create
+
+    end
+    
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def destroy
+
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users do
+    resources :characters, only: [:index, :show]
+  end
+
+  resources :campaigns do
+    resources :character, only: [:index, :show]
+  end
+
+  resources :characters
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
