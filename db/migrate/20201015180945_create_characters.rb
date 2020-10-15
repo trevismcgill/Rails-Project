7d7a880e9e0398cc70e0754d9827558d3a9f7094
trevismcgill <trevismcgill25@gmail.com>
2020-10-15 16:13:00 -0500

    Wrote out character migration

--- db/migrate/20201015180945_create_characters.rb
+++ db/migrate/20201015180945_create_characters.rb
@@ -1,7 +1,11 @@
 class CreateCharacters < ActiveRecord::Migration[6.0]
   def change
     create_table :characters do |t|
-
+      t.string :name
+      t.string :class
+      t.string :race
+      t.integer :user_id
+      t.integer :campaign_id
       t.timestamps
     end
   end
