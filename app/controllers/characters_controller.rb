b8cd99e43f3c4e60035a0a52f55539a0b12d4b72
trevismcgill <trevismcgill25@gmail.com>
2020-10-15 22:11:58 -0500

    Implemented Character Creator

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -12,29 +12,52 @@ class CharactersController < ApplicationController
     end
 
     def create
-
+        @character = Character.new(character_params)
+        @character[:user_id] = current_user.id
+        if @character.save
+            redirect_to character_path
+        else
+            flash.alert = @character.errors.full_messages
+            redirect_to new_character_path
+        end
     end
     
     def edit
-
+        if logged_in?
+            @character = Character.find_by_id(params[:id])
+        else
+            flash.alert = "You must be logged in!"
+            redirect_to login_path
+        end
     end
 
     def update
-
+        @character = Character.find_by_id(params[:id])
+        if @character.user_id == current_user.id
+            @character.update(character_params)
+            redirect_to character_path
+        else
+            flash.alert = "This is not your character!"
+            redirect_to login_path
     end
 
     def index
-
+        @characters = Character.all
     end
 
     def show
-
+        @character = Character.find_by_id(params[:id])
     end
 
     def destroy
-
+        @character = Character.find_by_id(params[:id])
+        @character.destroy
     end
 
     private
+
+    def character_params
+        params.require(:character).permit(:name, :race, :character_class, :level)
+    end
 
 end
