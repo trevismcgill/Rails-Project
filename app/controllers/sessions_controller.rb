bfe19768b2275be443dd06362984d4bfec60ea9e
trevismcgill <68857483+trevismcgill@users.noreply.github.com>
2020-10-27 16:41:29 -0500

    spacing was ugly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,19 +2,19 @@ class SessionsController < ApplicationController
 
     def create_by_omniauth
         @user = User.find_or_create_via_omniauth(auth)
-                set_session
-                redirect_to user_path(@user)
+        set_session
+        redirect_to user_path(@user)
     end
 
     def create_by_login
-            @user = User.find_by(:email => params[:user][:email])
-            if @user && @user.authenticate(params[:user][:password])
-                set_session
-                redirect_to user_path(@user)
-            else
-                 flash.alert = "Something went wrong!"
-                 redirect_to login_path
-            end
+        @user = User.find_by(:email => params[:user][:email])
+        if @user && @user.authenticate(params[:user][:password])
+            set_session
+            redirect_to user_path(@user)
+        else
+            flash.alert = "Something went wrong!"
+            redirect_to login_path
+        end
     end
 
     def destroy
